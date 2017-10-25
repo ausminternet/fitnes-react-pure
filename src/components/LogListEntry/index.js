a18651b60ba520384d3bacb5715b2f72d7551787
Jan Hofmann <jan.hofmann@dexite.de>
2017-10-25 04:12:18 +0200

    wip

--- src/components/LogListEntry/index.js
+++ src/components/LogListEntry/index.js
@@ -1,8 +1,6 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
 
-import BackIcon from 'media/icons/black/back-filled.png'
-
 import './styles.css'
 
 const LogListEntry = ({log}) => {
@@ -12,9 +10,7 @@ const LogListEntry = ({log}) => {
   const day = date.getDate()
   const year = date.getFullYear()
 
-  date = day + '.' + month + '.' + year
-
-  console.log(log)
+  const cleanDate = day + '.' + month + '.' + year
 
   return (
     <Link
@@ -23,9 +19,9 @@ const LogListEntry = ({log}) => {
       onTouchStart={() => true}
     >
       <div className="log-list-entry__arrow"></div>
-      <div className="log-list-entry__date">
-        {date}
-      </div>
+      <time className="log-list-entry__date" dateTime={date.toISOString()}>
+        {cleanDate}
+      </time>
       {log.type === 'random' &&
         <span className="log-list-entry__type log-list-entry__type--random">
             Random workout:
