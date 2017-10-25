36142474f8bbe794a052a762c26900c93af6847b
Jan Hofmann <jan.hofmann@dexite.de>
2017-10-25 03:50:24 +0200

    wip

--- src/components/PlannedWorkouts/index.js
+++ src/components/PlannedWorkouts/index.js
@@ -1,6 +1,9 @@
 import React from 'react'
 import StartWorkoutButton from 'components/StartWorkoutButton'
 import ContentHeader from 'components/ContentHeader'
+import {Link} from 'react-router-dom'
+
+import PrimaryButton from 'components/PrimaryButton'
 
 import './styles.css'
 
@@ -9,6 +12,22 @@ const PlannedWorkouts = () => {
     <div className="planned-workouts">
       <ContentHeader>Planned Workouts</ContentHeader>
       <div className="planned-workouts__buttons">
+        <PrimaryButton>
+          <button
+            to="/"
+            onTouchStart={() => true}
+          >
+            this is a &lt;button&gt;
+          </button>
+        </PrimaryButton>
+        <PrimaryButton>
+          <Link
+            to="/"
+            onTouchStart={() => true}
+          >
+            this is a &lt;Link&gt;
+          </Link>
+        </PrimaryButton>
         <StartWorkoutButton to="/workout/random/">
           upper body
         </StartWorkoutButton>
